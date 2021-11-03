4a96095d109641ed66b2feb8d0f82aed7c6c8fcb
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2021-11-03 16:44:34 +0900

    feat: add Modinv.cpp

---
+++ C++/Modinv.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long modinv(long long a, long long m) {
+  long long b = m, u = 1, v = 0;
+  while (b) {
+    long long t = a / b;
+    a -= t * b;
+    swap(a, b);
+    u -= t * v;
+    swap(u, v);
+  }
+  u %= m;
+  if (u < 0) u += m;
+  return u;
+}
