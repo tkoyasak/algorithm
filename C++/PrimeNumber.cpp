4a6892537052c74b3d1109e6f1b53bb27e9d2508
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2021-11-27 02:22:14 +0900

    refactor: change return value's type

--- C++/PrimeNumber.cpp
+++ C++/PrimeNumber.cpp
@@ -2,20 +2,15 @@
 using namespace std;
 
 // Sieve of eratosthenes to find prime numbers in range [1,n]
-vector<int> prime_number(int n) {
-  vector<int> res;
-  vector<bool> is_prime(n + 1, true);
+vector<bool> prime_number(int n) {
+  vector<bool> res(n + 1, true);
 
   int m = ceil(sqrt(n));
   for (int i = 2; i <= m; ++i) {
-    if (is_prime[i]) {
+    if (res[i]) {
       for (int j = i * 2; j <= n; j += i)
-        is_prime[j] = false;
+        res[j] = false;
     }
-  }
-  for (int i = 2; i <= n; ++i) {
-    if (is_prime[i])
-      res.push_back(i);
   }
 
   return res;
