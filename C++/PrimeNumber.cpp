c1d457f08a60078cea2ffe61583367f1713b87db
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2021-12-14 16:29:45 +0900

    fix: bug - fix to return 'false' when n==0 or n==1

--- C++/PrimeNumber.cpp
+++ C++/PrimeNumber.cpp
@@ -5,6 +5,7 @@ using namespace std;
 vector<bool> prime_number(int n) {
   vector<bool> res(n + 1, true);
 
+  res[0] = res[1] = false;
   int m = ceil(sqrt(n));
   for (int i = 2; i <= m; ++i) {
     if (res[i]) {
