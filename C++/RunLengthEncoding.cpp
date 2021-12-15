ff830f46a400bc338f80a43826b000372334e9b9
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2021-12-15 21:30:54 +0900

    feat: add RunLengthEncoding.cpp

---
+++ C++/RunLengthEncoding.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<pair<int, int>> run_length_encoding(vector<int> arr) {
+  vector<pair<int, int>> res;
+
+  int m = arr.size(), cnt = 1;
+  for (int i = 1; i < m; i++) {
+    if (arr[i] == arr[i - 1]) {
+      cnt++;
+    } else {
+      res.emplace_back(arr[i - 1], cnt);
+      cnt = 1;
+    }
+  }
+  res.emplace_back(arr[m - 1], cnt);
+
+  return res;
+}
