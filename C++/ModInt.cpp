88b3e1e79d6310c23314b3fcde6045d19ed406c2
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2021-12-18 16:06:37 +0900

    feat: add ModInt.cpp

---
+++ C++/ModInt.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<int MOD>
+class ModInt {
+ public:
+  long x;
+  explicit ModInt(long x = 0) : x((x % MOD + MOD) % MOD) {
+  }
+
+  ModInt& operator+=(const ModInt& other) {
+    if ((x += other.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  ModInt& operator-=(const ModInt& other) {
+    if ((x += MOD - other.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  ModInt& operator*=(const ModInt& other) {
+    (x *= other.x) %= MOD;
+    return *this;
+  }
+  ModInt& operator/=(const ModInt& other) {
+    return *this *= other.inv();
+  }
+
+  ModInt operator+(const ModInt& other) const {
+    return ModInt(*this) += other;
+  }
+  ModInt operator-(const ModInt& other) const {
+    return ModInt(*this) -= other;
+  }
+  ModInt operator*(const ModInt& other) const {
+    return ModInt(*this) *= other;
+  }
+  ModInt operator/(const ModInt& other) const {
+    return ModInt(*this) /= other;
+  }
+
+  bool operator==(const ModInt& other) const {
+    return x == other.x;
+  }
+  bool operator!=(const ModInt& other) const {
+    return x != other.x;
+  }
+
+  ModInt pow(long t) const {
+    if (!t) return 1;
+    ModInt a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+  ModInt inv() const {
+    return pow(MOD - 2);
+  }
+};
+
+template<int MOD> ostream& operator<<(ostream& os, const ModInt<MOD>& ans) {
+  return os << ans.x;
+}
+
+typedef ModInt<1000000007> mint;
