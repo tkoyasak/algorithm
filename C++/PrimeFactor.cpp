e490c7a99773255b0dbff7ace1041cc7827a4972
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2021-12-18 14:24:52 +0900

    refactor: args 'int' -> 'long'

--- C++/PrimeFactor.cpp
+++ C++/PrimeFactor.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<int, int> prime_factor(int n) {
-  map<int, int> res;
+map<long, int> prime_factor(long n) {
+  map<long, int> res;
 
-  int m = ceil(sqrt(n));
-  for (int i = 2; i <= m; ++i) {
+  for (long i = 2; i * i <= n; ++i) {
     while (n % i == 0) {
       res[i]++;
       n /= i;
     }
-    if (i > n) break;
   }
   if (n > 1) res[n]++;
 
