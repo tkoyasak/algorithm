1449c00a8aa73da5c418ede51c847fbb8dc7260b
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2021-12-18 14:25:21 +0900

    feat: add Combination.cpp

---
+++ C++/Combination.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T, int N>
+class Combination {
+ private:
+  vector<T> fac, inv;
+
+ public:
+  Combination(void) : fac(N, 1), inv(N, 1) {
+    for (int i = 1; i < N; ++i)
+      fac[i] = fac[i - 1] * i;
+    inv[N - 1] = T(1) / fac[N - 1];
+    for (int i = N - 2; i >= 1; --i)
+      inv[i] = inv[i + 1] * T(i + 1);
+  }
+
+  T npk(int n, int k) {
+    if (k < 0 || n < k) return 0;
+    return fac[n] * inv[n - k];
+  }
+
+  T nck(int n, int k) {
+    if (k < 0 || n < k) return 0;
+    return fac[n] * inv[n - k] * inv[k];
+  }
+
+  T nhk(int n, int k) {
+    if (n == 0 && k == 0) return 1;
+    if (n <= 0 || k < 0) return 0;
+    return nck(n + k - 1, k);
+  }
+};
