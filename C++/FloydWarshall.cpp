707c82b178b4d7db6d18cae912093f1fb965c18b
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2022-01-19 22:19:31 +0900

    feat: add function 'negative','get_path'

--- C++/FloydWarshall.cpp
+++ C++/FloydWarshall.cpp
@@ -5,21 +5,31 @@ class FloydWarshall {
  private:
   const long INF = 1LL << 60;
   int v;
-  vector<vector<long>> d;
+  vector<vector<long>> dp;
 
  public:
-  explicit FloydWarshall(int v) : v(v), d(v, vector<long>(v, INF)) {
-    for (int i = 0; i < v; ++i) d[i][i] = 0;
+  explicit FloydWarshall(int v) : v(v), dp(v, vector<long>(v, INF)) {
+    for (int i = 0; i < v; ++i) dp[i][i] = 0;
   }
 
   void add_edge(int from, int to, long cost) {
-    d[from][to] = cost;
+    dp[from][to] = cost;
   }
 
   void solve(void) {
     for (int k = 0; k < v; ++k)
       for (int i = 0; i < v; ++i)
         for (int j = 0; j < v; ++j)
-          d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+          dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+  }
+
+  bool negative(void) {
+    for (int i = 0; i < v; ++i)
+      if (dp[i][i] < 0) return true;
+    return false;
+  }
+
+  vector<vector<long>> get_path(void) {
+    return dp;
   }
 };
