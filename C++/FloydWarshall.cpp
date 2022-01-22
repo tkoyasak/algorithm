0e9a01c35b9efe08924865235e99f771cbf33b10
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2022-01-22 18:57:54 +0900

    fix: the update for invalid paths

--- C++/FloydWarshall.cpp
+++ C++/FloydWarshall.cpp
@@ -22,10 +22,14 @@ class FloydWarshall {
   }
 
   void solve(void) {
-    for (int k = 0; k < v; ++k)
-      for (int i = 0; i < v; ++i)
-        for (int j = 0; j < v; ++j)
+    for (int k = 0; k < v; ++k) {
+      for (int i = 0; i < v; ++i) {
+        for (int j = 0; j < v; ++j) {
+          if (dp[i][k] == INF || dp[k][j] == INF) continue;
           dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+        }
+      }
+    }
   }
 
   bool negative(void) {
