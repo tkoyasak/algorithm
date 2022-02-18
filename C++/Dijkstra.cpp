a0a07d59607c7fe265095149111d8177c2ea6efa
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-02-18 20:06:40 +0900

    refactor: initialize dp and prev in solve function

--- C++/Dijkstra.cpp
+++ C++/Dijkstra.cpp
@@ -1,48 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Edge {
- public:
-  int from, to;
-  long cost;
-  explicit Edge(int from, int to, long cost) : from(from), to(to), cost(cost) {}
-};
-
 class Dijkstra {
  private:
   const long INF = 1LL << 60;
   int v;
   vector<long> dp;
   vector<int> prev;
-  vector<vector<Edge>> es;
+  vector<vector<pair<int, long>>> es;
 
  public:
-  explicit Dijkstra(int v) : v(v), dp(v, INF), prev(v, -1), es(v) {}
+  explicit Dijkstra(int v) : v(v), es(v) {}
 
   void input(int e) {
     for (int i = 0; i < e; ++i) {
       int from, to;
       long cost;
       cin >> from >> to >> cost;
-      es[from].emplace_back(from, to, cost);
+      es[from].emplace_back(to, cost);
     }
   }
 
   void solve(int s) {
+    dp.assign(v, INF);
+    prev.assign(v, -1);
     priority_queue<pair<long, int>, vector<pair<long, int>>, greater<pair<long, int>>> q;
     dp[s] = 0;
     q.emplace(dp[s], s);
     while (!q.empty()) {
-      auto [cost, cur] = q.top(); q.pop();
-      if (dp[cur] < cost) continue;
+      auto [d, cur] = q.top(); q.pop();
+      if (dp[cur] < d) continue;
       for (auto u : es[cur]) {
-        if (dp[u.to] > dp[cur] + u.cost) {
-          dp[u.to] = dp[cur] + u.cost;
-          prev[u.to] = cur;
-          q.emplace(dp[u.to], u.to);
+        long cost = dp[cur] + u.second;
+        if (dp[u.first] > cost) {
+          dp[u.first] = cost;
+          prev[u.first] = cur;
+          q.emplace(dp[u.first], u.first);
         }
       }
     }
+  }
+
+  void output(void) {
+    // 
   }
 
   vector<int> get_path(int t) {
