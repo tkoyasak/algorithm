cd9c4776bd6ca4a92bc27a6f0ec9e9b0369a14d8
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-02-18 20:01:59 +0900

    refactor: manage function as class

---
+++ C++/Lis.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T>
+class Lis {
+ public:
+  vector<T> lis, sz;
+
+  Lis(vector<T>& arr, bool is_dup = false) {
+    for(auto x : arr) {
+      if (lis.size() == 0) lis.emplace_back(x);
+      else {
+        if (is_dup) {
+          if (x >= lis.back()) lis.emplace_back(x);
+          else *upper_bound(lis.begin(), lis.end(), x) = x;
+        }
+        else {
+          if (x > lis.back()) lis.emplace_back(x);
+          else *lower_bound(lis.begin(), lis.end(), x) = x;
+        }
+      }
+      sz.emplace_back(lis.size());
+    }
+  }
+};
