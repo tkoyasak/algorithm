9359a30024dab808201595eb8234e3ef381626d9
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-02-27 22:30:51 +0900

    refactor: change function name

--- C++/Rle.cpp
+++ C++/Rle.cpp
@@ -3,17 +3,13 @@ using namespace std;
 using ll = long long;
 
 template<class T>
-vector<pair<int, int>> run_length_encoding(T& arr) {
+vector<pair<int, int>> rl_encoding(T& arr) {
   vector<pair<int, int>> res;
 
   int m = arr.size(), cnt = 1;
   for (int i = 1; i < m; i++) {
-    if (arr[i] == arr[i - 1]) {
-      cnt++;
-    } else {
-      res.emplace_back(arr[i - 1], cnt);
-      cnt = 1;
-    }
+    if (arr[i] == arr[i - 1]) cnt++;
+    else res.emplace_back(arr[i - 1], cnt), cnt = 1;
   }
   res.emplace_back(arr[m - 1], cnt);
 
