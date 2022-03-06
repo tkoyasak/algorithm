5451185784a8fd183b2022fe36b1b0be7dc4679d
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-03-06 22:19:22 +0900

    docs: style change

--- tpl/tpl.cpp
+++ tpl/tpl.cpp
@@ -16,12 +16,12 @@ const long long INF=1e18;
 // const int mod=998244353;
 // const int mod=1e9+7;
 
-#ifndef tkoyasak_template
+#ifndef MEOW
 
-const int dx4[4]={1,0,-1,0};
-const int dy4[4]={0,1,0,-1};
-const int dx8[8]={1,1,0,-1,-1,-1,0,1};
-const int dy8[8]={0,1,1,1,0,-1,-1,-1};
+const int di4[4]={1,0,-1,0};
+const int dj4[4]={0,1,0,-1};
+const int di8[8]={1,1,0,-1,-1,-1,0,1};
+const int dj8[8]={0,1,1,1,0,-1,-1,-1};
 
 #define endl '\n'
 #define ft first
@@ -30,7 +30,7 @@ const int dy8[8]={0,1,1,1,0,-1,-1,-1};
 #define pf push_front
 #define eb emplace_back
 #define bit(n) (1LL<<(n))
-#define pcnt(n) __builtin_popcount(n)
+#define pcnt(n) __builtin_popcountll(n)
 #define all(obj) (obj).begin(),(obj).end()
 #define rall(obj) (obj).rbegin(),(obj).rend()
 #define sz(obj) (int)(obj).size()
@@ -41,22 +41,21 @@ const int dy8[8]={0,1,1,1,0,-1,-1,-1};
 
 #define overload4(o1,o2,o3,o4,name,...) name
 #define rep1(n) for (int rn4wqu22=0;rn4wqu22<(n);++rn4wqu22)
-#define rep2(i,n) for (int i=0;i<(n);++i)
-#define rep3(i,m,n) for (int i=(m);i<(n);++i)
-#define rep4(i,m,n,d) for (int i=(m);i<(n);i+=(d))
+#define rep2(rn4wqu22,n) for (int rn4wqu22=0;rn4wqu22<(n);++rn4wqu22)
+#define rep3(rn4wqu22,m,n) for (int rn4wqu22=(m);rn4wqu22<(n);++rn4wqu22)
+#define rep4(rn4wqu22,m,n,d) for (int rn4wqu22=(m);rn4wqu22<(n);rn4wqu22+=(d))
 #define rep(...) overload4(__VA_ARGS__,rep4,rep3,rep2,rep1)(__VA_ARGS__)
 #define per1(n) for (int rn4wqu22=(n)-1;rn4wqu22>=0;--rn4wqu22)
-#define per2(i,n) for (int i=(n)-1;i>=0;--i)
-#define per3(i,m,n) for (int i=(m)-1;i>=n;--i)
-#define per4(i,m,n,d) for (int i=(m)-1;i>=n;i-=(d))
+#define per2(rn4wqu22,n) for (int rn4wqu22=(n)-1;rn4wqu22>=0;--rn4wqu22)
+#define per3(rn4wqu22,m,n) for (int rn4wqu22=(m)-1;rn4wqu22>=n;--rn4wqu22)
+#define per4(rn4wqu22,m,n,d) for (int rn4wqu22=(m)-1;rn4wqu22>=n;rn4wqu22-=(d))
 #define per(...) overload4(__VA_ARGS__,per4,per3,per2,per1)(__VA_ARGS__)
-#define fore(i,a) for(auto &i:(a))
+#define fore(rn4wqu22,a) for(auto &rn4wqu22:(a))
 
 template<class T> inline bool chmin(T& a, T b) {if(a>b){a=b;return(true);}return(false);}
 template<class T> inline bool chmax(T& a, T b) {if(a<b){a=b;return(true);}return(false);}
 long long modpow(long long x,long long n,long long m) {if(m==1)return(0);long long val=1;while(n){if(n&1){val=(val*x)%m;}x=(x*x)%m;n>>=1;}return(val);}
 long long modinv(long long x,long long m) {return(modpow(x,m-2,m));}
-
 
 #ifndef MODINT
  #define mint int
@@ -73,15 +72,13 @@ using pii=pair<int,int>; using pll=pair<ll,ll>; using pil=pair<int,ll>; using pl
 using vpii=vector<pii>; using vpll=vector<pll>; using vpil=vector<pil>; using vpli=vector<pli>;
 using pdd=pair<double,double>; using vpdd=vector<pdd>;
 
+#define NYAN cin.tie(nullptr);ios_base::sync_with_stdio(false);cout<<fixed<<setprecision(15);
+
 #endif
 
 int	main(void)
 {
-  /* ---------- ---------- ---------- */
-  cin.tie(nullptr);
-  ios_base::sync_with_stdio(false);
-  cout<<fixed<<setprecision(15);
-  /* ---------- ---------- ---------- */
+  NYAN;
 
   
 }
